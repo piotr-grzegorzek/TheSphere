ceaeecc4ce606e676c72339ee74c74275ac67482
SciCreed <49654876+SciCreed@users.noreply.github.com>
2023-10-25 08:24:22 +0200

    feat: jump

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,6 +6,8 @@ namespace Player
     {
         public float Thrust = 5.0f;
 
+        public float JumpForce = 100.0f;
+
         private Vector3 _axis;
 
         void Update()
@@ -13,6 +15,7 @@ namespace Player
             // Input can be configured in Edit > Project Settings > Input Manager > Axes
             // According to docs they are framerate independent, so no time.deltaTime needed
             _axis.x = Input.GetAxis("Horizontal");
+            _axis.y = (Input.GetKey(KeyCode.Space) && transform.position.y <= 0.5f) ? JumpForce : 0.0f;
             _axis.z = Input.GetAxis("Vertical");
         }
 
