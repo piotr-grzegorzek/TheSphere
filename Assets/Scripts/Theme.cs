d46ca94762387da96cf0e8bd051ce83ea3faeb38
SciCreed <49654876+SciCreed@users.noreply.github.com>
2023-10-26 22:52:08 +0200

    refactor: project elements naming, structure, no singleton in theme

--- Assets/Scripts/Theme.cs
+++ Assets/Scripts/Theme.cs
@@ -4,14 +4,6 @@ public class Theme : MonoBehaviour
 {
     void Awake()
     {
-        // This is a singleton pattern. It ensures that only one instance of this object exists at any given time
-        // Destroy this object if another object with the "Music" tag already exists
-        var obj = GameObject.FindGameObjectWithTag("Music");
-        if (obj != null && obj != gameObject)
-        {
-            Destroy(gameObject);
-        }
-
         // Don't destroy this object when loading new scenes
         DontDestroyOnLoad(gameObject);
     }
